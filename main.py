287880bc1a88aceba469fcb4378378c47820ec27
qompu <40707870+qompu@users.noreply.github.com>
2022-02-06 06:22:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Reference:
+# Getting Started with Python and MongoDB | MongoDB Blog
+# https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb
+#
+# YAML with Python
+# https://blog.devgenius.io/yaml-with-python-d6787a9bd8ab
