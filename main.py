ea2bb9ce5084c4243e1e5fd3de67a728afacee7b
qompu <40707870+qompu@users.noreply.github.com>
2022-02-06 06:38:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,4 @@
 # YAML with Python
 # https://blog.devgenius.io/yaml-with-python-d6787a9bd8ab
 
-# Exploring Collections and Documents in JSON (https://www.mongodb.com/blog/post/getting-started-with-python-and-mongodb)
-![jasondoc.png](jasondoc.png)
-
-
-<p align="center">
-  <img src="jasondoc.png" width="350" title="JSON doc" alt="JSON doc">
-</p>
 
